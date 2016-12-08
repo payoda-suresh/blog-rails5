0b3135b0570779e3bfcf4566336fbe6b9d0e37f9
vsureshk <vsureshk321@gmail.com>
2016-12-08 16:12:51 +0530

    Add comments and articles code refactor

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-  validate :title, presence:true, length: {minimum: 5}
+  has_many :comments, dependent: :destroy
+  validates :title, presence:true, length: {minimum: 5}
 end
